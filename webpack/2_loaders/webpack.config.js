2a1b99ee94db08394b0d8d9a8cef1610c81359c3
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-11-29 11:01:53 -0300

    importando sass

--- webpack/2_loaders/webpack.config.js
+++ webpack/2_loaders/webpack.config.js
@@ -17,7 +17,15 @@ module.exports = {
             },
             {
                 test: /\.(png|jpg|jpeg)$/,
-                use: 'file-loader'
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader'
+                ]
             }
         ]
     }
