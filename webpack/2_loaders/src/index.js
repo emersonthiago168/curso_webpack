aa8caf242d82a4739f30e0bbc5c8570a3b10d222
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-11-29 11:16:27 -0300

    importando sass

--- webpack/2_loaders/src/index.js
+++ webpack/2_loaders/src/index.js
@@ -1,8 +1,12 @@
 import Title from './components/title.js';
 import Image from './components/image.js';
+import Button from './components/button.js';
 
 const title = new Title();
 title.create('Primeira página');
 
 const image = new Image();
 image.insertImage();
+
+const button = new Button();
+button.create()
