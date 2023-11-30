010a933acc4c49224feb6ca84241c8a8dbbbbae6
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-11-30 10:51:24 -0300

    gerando arquivo html

--- webpack/3_plugins/webpack.config.js
+++ webpack/3_plugins/webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const webpack = require('webpack');
-const DotenvPlugin = require('dotenv-webpack')
+const DotenvPlugin = require('dotenv-webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -33,6 +34,7 @@ module.exports = {
             VERSION: JSON.stringify('1.0.2'),
             PORT: JSON.stringify('8080'),
         }),
-        new DotenvPlugin()
+        new DotenvPlugin(),
+        new HTMLWebpackPlugin()
     ]
 }
