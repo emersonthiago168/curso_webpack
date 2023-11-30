943ab8654e223221af0554b64f84eba2b039dc03
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-11-30 10:40:23 -0300

    definindo constantes globais

--- webpack/3_plugins/src/index.js
+++ webpack/3_plugins/src/index.js
@@ -2,3 +2,7 @@ import Heading from './components/headings/heading';
 
 const heading = new Heading();
 heading.create('Página principal');
+
+console.log(VERSION);
+console.log(PORT);
+console.log(nome);
