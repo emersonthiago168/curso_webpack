03f3311737a43802b9e55d063cb722def9f19591
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-12-06 14:58:32 -0300

    integrando o react

---
+++ webpack/7_integracao_react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.css';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Meu projeto em React e Webpack</h1>
+                <p>Integrado com sucesso!</p>
+            </div>
+        );
+    }
+}
