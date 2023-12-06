db3c97721130362ecbf64fd603cf6b2d0c6aa7cb
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-12-06 11:38:14 -0300

    integrando fonte de ícone

--- webpack/5_integracoes/src/index.js
+++ webpack/5_integracoes/src/index.js
@@ -4,6 +4,14 @@ import './fonts.css';
 // IMPORT JQUERRY
 import $ from 'jquery';
 
+// IMPORT FONT AWESOME
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faBacon, faAnchor } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBacon);
+library.add(faAnchor);
+dom.watch();
+
 const body = $('body');
 const p = $("<p></p>").text("Inserindo texto").css("color", "red");
 
