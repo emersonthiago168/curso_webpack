fa0ce55ad42d6adea71d794e3aa6802f9fe5640c
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-12-06 16:11:58 -0300

    integrando o Vue

---
+++ webpack/8_integracao_vue/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <div id="app">
+        <div class="nav">
+            <rounter-link to="/">Home</rounter-link> | <rounter-link to="/about">About</rounter-link>
+        </div>
+        <rounter-view />    
+    </div>
+</template>
