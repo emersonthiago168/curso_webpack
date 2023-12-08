0b5d5ed3bef365b093e2e6ecf7ff04984556dbd7
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-12-08 14:42:53 -0300

    shimming

---
+++ webpack/11_recursos_webpack/src/index.js
@@ -0,0 +1,3 @@
+console.log(_.difference([1, 2, 3], [4, 5]));
+
+console.log(_.lastIndexOf([1, 2, 3], 2));
---
+++ webpack/11_recursos_webpack/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    mode: 'production',
+    plugins: [
+        new webpack.ProvidePlugin({
+            _: 'lodash',
+        })
+    ]
+}
