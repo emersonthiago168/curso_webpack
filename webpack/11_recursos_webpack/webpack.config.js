a0cca5a009c48c4a737ea636c655309d4dc8bd05
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-12-08 15:21:49 -0300

    melhorando a perfomance de build

--- webpack/11_recursos_webpack/webpack.config.js
+++ webpack/11_recursos_webpack/webpack.config.js
@@ -1,16 +1,35 @@
 const path = require('path');
-const webpack = require('webpack')
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js'
+        filename: '[name].js'
+    },
+    devServer: {
+        historyApiFallback: true
     },
     mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: path.resolve(__dirname, 'src')
+            }
+        ]
+    },
     plugins: [
         new webpack.ProvidePlugin({
             _: 'lodash',
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Recursos Webpack'
         })
-    ]
+    ],
+    optimization: {
+        runtimeChunk: true
+    }
 }
